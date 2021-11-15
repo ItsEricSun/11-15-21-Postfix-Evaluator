280b47de82b56b9b96c2578652740a715d7a044c
ItsEricSun <ericsun828@gmail.com>
2021-11-15 09:42:36 -0500

    1

---
+++ src/LinkedStack.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class LinkedStack<E> implements Stack<E> {
+	private List<E> stack = new LinkedList<E>();
+
+	@Override
+	public void push(E elem) {
+		stack.addFirst(elem);
+	}
+
+	@Override
+	public E pop() {
+		return stack.removeFirst();
+	}
+
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	
+	
+
+}
